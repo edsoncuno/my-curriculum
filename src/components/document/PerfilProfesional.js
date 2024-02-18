7b63d5144d637c579ed76cc0168be291c044b628
Edson Cuno <74805638+edsoncuno@users.noreply.github.com>
2024-02-17 20:38:36 -0500

    Update PerfilProfesional.js

--- src/components/document/PerfilProfesional.js
+++ src/components/document/PerfilProfesional.js
@@ -1,7 +1,7 @@
 import styles from "./PerfilProfesional.module.css";
 
 export default function App(props) {
-  const perfil = "Egresado de ingeniería informática de la Universidad Ricardo Palma en búsqueda de una primera experiencia laboral a tiempo completo para poner en práctica sus conocimientos de ciencia de datos en el ambiente laboral, con disponibilidad inmediata.";
+  const perfil = "Estudiante del noveno ciclo de ingeniería informática de la Universidad Ricardo Palma en búsqueda de una primera experiencia laboral a tiempo completo para poner en práctica sus conocimientos de ciencia de datos en el ambiente laboral, con disponibilidad inmediata.";
 
   return (
     <div className={styles.app}>
